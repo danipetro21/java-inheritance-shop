3fe1f018b0541c835f5b79309fc4d4530e536580
danipetro21 <idany346@gmail.com>
2023-06-06 14:31:39 +0200

    base shop class

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,20 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+
+import org.lessons.java.helper.Helper;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double prezzoNetto = 200.00;
+        double iva = 0.22;
+
+
+        Shop televisione = new Shop("televisione", "55 policci", prezzoNetto, iva);
+
+        System.out.println(televisione.getFullName());
+        System.out.println("Prezzo netto: " + televisione.getPrezzo() + " €");
+        System.out.println("Prezzo con iva: " + Helper.getPrezzoIva(prezzoNetto, iva) + " €");
+
+    }
 }
--- src/org/lessons/java/shop/Shop.java
+++ src/org/lessons/java/shop/Shop.java
@@ -1,2 +1,67 @@
-package org.lessons.java.shop;public class Shop {
+package org.lessons.java.shop;
+
+import org.lessons.java.helper.Helper;
+
+import java.math.BigDecimal;
+
+public class Shop {
+
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private double iva;
+
+
+    public Shop(String nome, String descrizione, double prezzo, double iva) {
+        this.codice = Helper.generateCode();
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+
+    public String getFullName() {
+        return codice + "-" + nome;
+    }
+
+
 }
